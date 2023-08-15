2d78aa3a27640a5e4dcc3e2d84c8fc1ae013e651
Saniul haque <haquesaniul@gmail.com>
2023-08-15 11:00:17 +0530

    web frame

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_travel_ui/pages/details_screen.dart';
-import 'package:flutter_travel_ui/pages/home_screen.dart';
 import 'package:flutter_travel_ui/pages/splash_screen.dart';
+import 'package:flutter_web_frame/flutter_web_frame.dart';
 import 'package:window_manager/window_manager.dart' show WindowOptions, windowManager;
 import 'utils/platforms_details.dart';
 
@@ -29,10 +29,18 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: SplashScreen(),
-      ),
+    return FlutterWebFrame(
+      builder: (context) {
+        return const MaterialApp(
+          home: Scaffold(
+            body: SplashScreen(),
+          ),
+        );
+      },
+      clipBehavior: Clip.hardEdge,
+      maximumSize: const Size(375.0, 812.0),
+      enabled: kIsWeb,
+      backgroundColor: Colors.grey,
     );
   }
 }
